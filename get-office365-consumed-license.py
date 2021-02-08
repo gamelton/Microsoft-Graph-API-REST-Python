1850e2c5d5a244b7518246fff851873215a3edff
gamelton <43844774+gamelton@users.noreply.github.com>
2021-02-08 20:17:54 +0300

    Update get-office365-consumed-license.py

--- get-office365-consumed-license.py
+++ get-office365-consumed-license.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import requests
-import json
 payload = {'grant_type': 'client_credentials', 'client_id': '{APP-ID}', 'client_secret': '{APP-SECRET}', 'scope': 'https://graph.microsoft.com/.default'}
 tokenurl = 'https://login.microsoftonline.com/{TENANT-GUID}/oauth2/v2.0/token'
 enterprisepackskuurl = 'https://graph.microsoft.com/v1.0/subscribedSkus/{SKU-ID}'
